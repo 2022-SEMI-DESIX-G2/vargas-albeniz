9bd9e002099170189c450d2612da9d563f2c6839
Albeniz Vargas <albeniz1111@gmail.com>
2022-05-07 12:08:21 -0500

    Pokemon working fine, just missing abilities

---
+++ Lab6/index.js
@@ -0,0 +1,36 @@
+((Utils) => {
+  const App = {
+    htmlElements: {
+      pokemonFinderForm: document.querySelector("#pokemon-finder-form"),
+      pokemonFinderSearchType: document.querySelector(
+        "#pokemon-finder-search-type"
+      ),
+      pokemonFinderInput: document.querySelector("#pokemon-finder-query"),
+      pokemonFinderOutput: document.querySelector("#pokemon-finder-response"),
+    },
+    init: () => {
+      App.htmlElements.pokemonFinderForm.addEventListener(
+        "submit",
+        App.handlers.pokemonFinderFormOnSubmit
+      );
+    },
+    handlers: {
+      pokemonFinderFormOnSubmit: async (e) => {
+        e.preventDefault();
+
+        const query = App.htmlElements.pokemonFinderInput.value;
+        const searchType = App.htmlElements.pokemonFinderSearchType.value;
+        try {
+          const information = await Utils.getPokemon({
+            query,
+            searchType,
+          });
+          App.htmlElements.pokemonFinderOutput.innerHTML = `<div class="card"> <div class="card-header" style="background-image: url(${information.sprites.other.home.front_default})"></div><div class="card-body"><h2 class="name">${information.name}</h2><h4 class="type-title">Type:${information.types[0].type.name}</h4></div><div class="card-footer"><div class="stats"><div class="stat"><span class="label">Height</span><span class="value">${information.height}</span></div><div class="stat"><span class="label">Weight</span><span class="value">${information.weight}</span></div><div class="stat"><span class="label">ID#</span><span class="value">${information.id}</span></div></div></div></div>`;
+        } catch (error) {
+          App.htmlElements.pokemonFinderOutput.innerHTML = `<h1>${error}</h1>`;
+        }
+      },
+    },
+  };
+  App.init();
+})(document.Utils);
