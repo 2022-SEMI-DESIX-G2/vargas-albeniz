af6ed9d7bda9faecad31e7ad270ae8ae9b1fd5f7
Albeniz Vargas <albeniz1111@gmail.com>
2022-05-23 21:04:44 -0500

    Lab 6 Fibonacci

---
+++ lab6/app.js
@@ -0,0 +1,13 @@
+(function() {
+    const series = 4;
+    const fiboArr =[];
+    let n1 = 0, n2 = 1, nextTerm;
+    for (let i = 1; i <= series; i++) {
+
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+        fiboArr.push(n1);
+    }
+    console.log(fiboArr);
+ })();
